a3a15e5106efa84107fab8d0bdade92a85d0b8bc
Jehad <SwitcherDev@Jehad-Home>
2020-05-27 11:56:24 +0300

    second commit

--- resources/views/purchasing/purchasing-detail.blade.php
+++ resources/views/purchasing/purchasing-detail.blade.php
@@ -59,7 +59,7 @@
                 </h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
-                        <li class="breadcrumb-item">Purchasing</li>
+                        <li class="breadcrumb-item">Purchasing COOL</li>
                         <li class="breadcrumb-item" aria-current="page">
                             <a class="link-fx" href="">Job Order detail</a>
                         </li>
